a36b9b33688f7b02a9e4b811ecf3485d468ecb50
Shawon Akondo <shawonakondo20@gmail.com>
2026-08-16 20:28:40 +0600

    complete Task 3 with marks array methods (reduce, find, findIndex)

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ console.log(addNumber);
 let greaterThen30 = numbers.filter(num => num > 30);
 console.log(greaterThen30);
 
+
+
+/*-------------------------------
+Task 3 — reduce(), find(), findIndex()
+---------------------------------*/
+let marks = [40, 55, 70, 85, 30];
+
+//get the totalMarks using reduce() mathod;
+let totalMark = marks.reduce((total, mark) => total += mark, 0);
+console.log("Total Marks:", totalMark); 
+
+//Find the first mark greater than 70 using find() mathod
+let firstAbove70 = marks.find(mark => mark > 70);
+console.log("First mark greater then 70:", firstAbove70)
+
+//Find index of 85 using findIndex() mathod
+let indexOf85 = marks.findIndex(mark => mark === 85);
+console.log('index of 85 is', indexOf85);
+
