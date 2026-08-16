cd97fb5ad6fab1158f77b6984a52e72c4d2ad08c
Shawon Akondo <shawonakondo20@gmail.com>
2026-08-16 20:07:54 +0600

    complete Task 2 with numbers array methods (map, filter)

--- index.js
+++ index.js
@@ -11,4 +11,15 @@ fruits.pop();
 const fruit = fruits.forEach(fruit => console.log(fruit));
 
 
+/*-------------------------------
+Task 2 — map() & filter()
+---------------------------------*/
+let numbers = [10, 20, 30, 40, 50];
+// Add 10 to Each number using map
+const addNumber = numbers.map(num => num + 10);
+console.log(addNumber);
+
+// Use filter() to get numbers greater than 30 from the array
+let greaterThen30 = numbers.filter(num => num > 30);
+console.log(greaterThen30);
 
