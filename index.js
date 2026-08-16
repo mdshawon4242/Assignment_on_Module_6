135c4b9645d3438ea821277d5ff7d772de792be8
Shawon Akondo <shawonakondo20@gmail.com>
2026-08-16 19:46:11 +0600

    complete Task 1 with fruits array methods (push, pop, forEach)

---
+++ index.js
@@ -0,0 +1,14 @@
+//Task 1 — Array Methods
+let fruits = ['Apple', 'Mango', 'Banana'];
+
+//add orange to the end;
+fruits.push("Orange");
+
+//remove the last item of fruits
+fruits.pop();
+
+//Display all fruits in the console using forEach;
+const fruit = fruits.forEach(fruit => console.log(fruit));
+
+
+
