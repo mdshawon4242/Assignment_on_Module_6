b1a06f639413e3642d4d0f6f6a0921623e29f24d
Shawon Akondo <shawonakondo20@gmail.com>
2026-08-16 21:04:02 +0600

    Complete Task 7

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
 let fruits = ['Apple', 'Mango', 'Banana'];
 
 //add orange to the end;
-fruits.push("Orange");
+fruits.push('Orange');
 
 //remove the last item of fruits
 fruits.pop();
 
 //Display all fruits in the console using forEach;
 const fruit = fruits.forEach(fruit => console.log(fruit));
-
 
 /*-------------------------------
 Task 2 — map() & filter()
@@ -23,27 +22,22 @@ console.log(addNumber);
 let greaterThen30 = numbers.filter(num => num > 30);
 console.log(greaterThen30);
 
-
-
 /*-------------------------------
 Task 3 — reduce(), find(), findIndex()
 ---------------------------------*/
 let marks = [40, 55, 70, 85, 30];
 
 //get the totalMarks using reduce() mathod;
-let totalMark = marks.reduce((total, mark) => total += mark, 0);
-console.log("Total Marks:", totalMark); 
+let totalMark = marks.reduce((total, mark) => (total += mark), 0);
+console.log('Total Marks:', totalMark);
 
 //Find the first mark greater than 70 using find() mathod
 let firstAbove70 = marks.find(mark => mark > 70);
-console.log("First mark greater then 70:", firstAbove70)
+console.log('First mark greater then 70:', firstAbove70);
 
 //Find index of 85 using findIndex() mathod
 let indexOf85 = marks.findIndex(mark => mark === 85);
 console.log('index of 85 is', indexOf85);
-
-
-
 
 /*-------------------------------
 Task 4 — Object Methods
@@ -54,13 +48,13 @@ let student = {
   city: 'Chittagong',
 };
 
-console.log("keys:", Object.keys(student));
-console.log("Values:", Object.values(student));
-console.log("Entries:", Object.entries(student));
-console.log("Does student have 'name' property?:", student.hasOwnProperty("name"));
-
-
-
+console.log('keys:', Object.keys(student));
+console.log('Values:', Object.values(student));
+console.log('Entries:', Object.entries(student));
+console.log(
+  "Does student have 'name' property?:",
+  student.hasOwnProperty('name'),
+);
 
 /*-------------------------------
 Task 5 — Set
@@ -73,13 +67,10 @@ set_numbers.add(40);
 //Delete number in set using delete() mathod
 set_numbers.delete(20);
 
-console.log("Does set_number have 30?", set_numbers.has(30));
+console.log('Does set_number have 30?', set_numbers.has(30));
 
 //Display all value in the console using forEach
 set_numbers.forEach(num => console.log(num));
-
-
-
 
 /*-------------------------------
 Task 6 — Map
@@ -95,3 +86,23 @@ console.log(map_student.get('name'));
 
 //Get the total length of map_student using size
 console.log(map_student.size);
+
+/*-------------------------------
+Task 7 — Spread & Rest
+---------------------------------*/
+//Spread
+let a = [1, 2, 3];
+let b = [4, 5, 6];
+//Merge arrays a and b using spread operator
+let margeArray = [...a, ...b];
+console.log(margeArray);
+
+//Rest
+function sum(...rest) {
+  let sum = 0;
+  for (num of rest) {
+    sum += num;
+  }
+  return sum;
+}
+console.log("Total sum is:", sum(10, 20, 30));
